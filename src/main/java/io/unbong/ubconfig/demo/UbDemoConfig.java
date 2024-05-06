a061413fec8df5c4b4440f5741baa7e1b9a696d8
unbong <ecunbong@gmail.com>
2024-05-06 14:28:25 +0900

    client update property and heartbeat

--- src/main/java/io/unbong/ubconfig/demo/UbDemoConfig.java
+++ src/main/java/io/unbong/ubconfig/demo/UbDemoConfig.java
@@ -15,4 +15,6 @@ public class UbDemoConfig {
 
     String a;
 
+    String b;
+
 }
--- src/main/java/io/unbong/ubconfig/demo/UbconfigDemoApplication.java
+++ src/main/java/io/unbong/ubconfig/demo/UbconfigDemoApplication.java
@@ -8,14 +8,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigurationProperties({UbDemoConfig.class})
 @EnableUbConfig
+@RestController
 public class UbconfigDemoApplication {
 
     @Value("${ub.a}")
     private String a;
+
+    @Value("${ub.b}")
+    private String b;
+
+    @Value("${ub.c}")
+    private String c;
 
     @Autowired
     private UbDemoConfig config;
@@ -32,5 +41,13 @@ public class UbconfigDemoApplication {
             System.out.println(a);
             System.out.println(config.getA());
         };
+    }
+
+    @GetMapping("/demo")
+    public String hello(){
+        return "ub.a = " + a + "\n" +
+                "ub.b = " + b + "\n" +
+                "demo.a = " + config.getA() + "\n"+
+                "demo.b = " + config.getB() + "\n";
     }
 }
